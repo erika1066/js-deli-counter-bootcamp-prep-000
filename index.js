880764df8a08ecf03ab7e9ef9c68cda1ce3c5b9e
erika1066 <hastings.erika@gmail.com>
2016-11-21 02:21:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-//var katzDeli = [];
-//var katzDeliLine = ["Steven", "Blake", "Avi"];
-var otherDeli = ["Ada"]
-//, "Grace", "Kent"];
-//var otherDeli = ["Ada"]
 
 function takeANumber(katzDeliLine, otherDeli){
   var katzDeli = []
-  //var i = 1;
-  //i++;
-  //otherDeli.push(`"Grace", "Kent"`)
   katzDeliLine.push(otherDeli);
-  //console.log(katzDeliLine)
-  //katzDeliLine = [...katzDeliLine, "Grace", "Kent")
   for(let i = 0, l = katzDeliLine.length; i < l; i++){
     katzDeli.push(`Welcome, ${katzDeliLine[i++]}. You are number ${i++} in line.`)
   }
-  //otherDeli.push("Ada");
-  //katzDeli.push(`Welcome, ${otherDeli[]}. You are number ${i} in line.`);
-  //otherDeli.push("Grace", "Kent");
-  //katzDeliLine.push(otherDeli);
-  //return katzDeliLine;
   return katzDeli;
 }
+
+var noLine = [];
+var deliLine = ["Steven", "Blake", "Avi"];
+function nowServing(noLine, deliLine){
+  var nowServ = [];
+  if(!noLine.length){
+    return "There is nobody waiting to be served!";
+  }
+  for(let i = 0, l = deliLine.length; i < l; i++){
+    nowServ.push(`Now serving ${deliLine[i]}.`)
+  }
+  return nowServ;
+}
+
+//function currentLine(line){
+//  if(line = 0){
+//    return "The line is currently empty."
+//  }
+//}
